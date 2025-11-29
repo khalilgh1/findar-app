7ec0055c6dc3feaadc03eddc10f9fa158cee1415
youcef chouki <youcef.chouki@ensia.edu.dz>
2025-11-29 17:22:08 +0100

    post and its pictures model

--- findar_backend/api/models.py
+++ findar_backend/api/models.py
@@ -5,7 +5,13 @@ from django.db import models
 # Create your models here.
 
 
-################# Customized User 
+# changes to be made 
+"""
+    1 . pictures need to be have a random name
+"""
+
+
+################# Customized User Model
 
 ACCOUNT_CHOICES = [
     ('normal' , 'Normal'),
@@ -23,4 +29,28 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
-###################################
+    
+################# Post Model
+
+class Post(models.Model):
+
+    owner        = models.ForeignKey(CustomUser , on_delete=models.CASCADE)
+    titel        = models.CharField(max_length=255)
+    price        = models.FloatField()
+    description  = models.TextField(max_length=1000) 
+    created_at   = models.DateTimeField(auto_now=True)
+    active       = models.BooleanField(default=True)
+    boosted      = models.BooleanField(default=False)
+    main_pic  = models.ImageField(upload_to="profiles/" , blank=True , null=True)
+    latitude     = models.FloatField()
+    longitude    = models.FloatField()
+
+    def __str__(self):
+        return f"{self.owner} : {self.titel}"
+
+################# Photos Model
+
+class Photos(models.Model):
+
+    post    = models.ForeignKey(Post)    
+    picture = models.ImageField(upload_to="profiles/" , blank=True , null=True)
