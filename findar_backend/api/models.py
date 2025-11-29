4dc0182c447c74932d48a7a388241e3f6c16f72f
youcef chouki <youcef.chouki@ensia.edu.dz>
2025-11-29 17:09:15 +0100

    custom user model

--- findar_backend/api/models.py
+++ findar_backend/api/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
 # Create your models here.
+
+
+################# Customized User 
+
+ACCOUNT_CHOICES = [
+    ('normal' , 'Normal'),
+    ('agency' , 'Agency')
+]
+
+class CustomUser(AbstractUser):
+
+    phone_number    = models.CharField(max_length=15)
+    profile_pic     = models.ImageField(upload_to="profiles/" , blank=True , null=True)
+    account_type    = models.CharField(max_length=20, choices=ACCOUNT_CHOICES, default='user')
+    credits         = models.FloatField(default=0.0) 
+
+
+    def __str__(self):
+        return self.username
+    
+###################################
