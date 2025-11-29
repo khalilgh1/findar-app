58238f3e17c599c56b8b425bcdfa7a434f67258a
youcef chouki <youcef.chouki@ensia.edu.dz>
2025-11-29 17:32:18 +0100

    report saved posts model

--- findar_backend/api/models.py
+++ findar_backend/api/models.py
@@ -7,7 +7,8 @@ from django.db import models
 
 # changes to be made 
 """
-    1 . pictures need to be have a random name
+    1 . pictures need to be have a random name (post and profile)
+    2 . Report model needs to be bigger
 """
 
 
@@ -32,6 +33,19 @@ class CustomUser(AbstractUser):
     
 ################# Post Model
 
+LISTING_TYPE_CHOICES = [
+    ('rent' , 'Rent'),
+    ('sale' , 'Sale')
+]
+
+BUILDING_TYPE_CHOICES = [
+    ('apartment' , 'Apartment'),
+    ('house'     , 'House'),
+    ('studio'    , 'Studio'),
+    ('villa'     , 'Villa'),
+    ('office'    , 'Office'),
+]
+
 class Post(models.Model):
 
     owner        = models.ForeignKey(CustomUser , on_delete=models.CASCADE)
@@ -41,9 +55,15 @@ class Post(models.Model):
     created_at   = models.DateTimeField(auto_now=True)
     active       = models.BooleanField(default=True)
     boosted      = models.BooleanField(default=False)
-    main_pic  = models.ImageField(upload_to="profiles/" , blank=True , null=True)
+    main_pic     = models.ImageField(upload_to="profiles/" , blank=True , null=True)
     latitude     = models.FloatField()
     longitude    = models.FloatField()
+    bedrooms     = models.IntegerField(default=0  , null=True)
+    bathrooms    = models.IntegerField(default=0  , null=True)
+    livingrooms  = models.IntegerField(default=0  , null=True)
+    area         = models.FloatField(default=0.0  , null=True)
+    listingtype  = models.CharField(max_length=50 , choices=LISTING_TYPE_CHOICES, null=True)  
+    buildingtype = models.CharField(max_length=50 , choices=BUILDING_TYPE_CHOICES, null=True)  
 
     def __str__(self):
         return f"{self.owner} : {self.titel}"
@@ -54,3 +74,28 @@ class Photos(models.Model):
 
     post    = models.ForeignKey(Post)    
     picture = models.ImageField(upload_to="profiles/" , blank=True , null=True)
+
+
+################# SavedPosts Model
+
+class SavedPosts(models.Model):
+
+    user    = models.ForeignKey(CustomUser)
+    post    = models.ForeignKey(Post)    
+    saved_at= models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} : {self.post}"
+    
+################# Report Model
+
+class Report(models.Model):
+
+    reporter_user    = models.ForeignKey(CustomUser)
+    reported_user    = models.ForeignKey(CustomUser)
+    post             = models.ForeignKey(Post , null=True , blank=True)   
+    description      = models.TextField(max_length=1000) 
+    created_at       = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.reporter_user} reported {self.reported_user}"
