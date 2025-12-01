1fc59183bd3e87f1899870c8e9099d75e6700940
KhalilGH <khalil.ghanem@ensia.edu.dz>
2025-12-01 14:15:46 +0100

    Added some end points setup

--- findar_backend/api/views.py
+++ findar_backend/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from .models import *
+from .serializers import *
+from django.db.models import Q
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+#########LISTINGS VIEW#########
+
+
+@api_view(['GET'])
+def get_listings(request):
+    posts = Post.objects.all()
+    serializer = PostSerializers(posts, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_listing(request):
+    print(request)
+    serializer = PostSerializers(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+
+
+#########User VIEW#########
