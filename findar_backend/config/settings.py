d3d1302b3b26f2164fff88b625e4e941a21e10b6
Youcef-ch <youcef.chouki@ensia.edu.dz>
2026-01-03 22:55:46 +0100

    adding essential configurations to settings

--- findar_backend/config/settings.py
+++ findar_backend/config/settings.py
@@ -144,3 +144,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = "/media/" 
 MEDIA_ROOT = BASE_DIR / "media"  
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = #sender's email-id
+EMAIL_HOST_PASSWORD = #password associated with above email-id (not the regular password)
