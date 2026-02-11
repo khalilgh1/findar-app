0df67a383e8b550109d73c7c1c66868dcc1ba48e
KhalilGH <khalil.ghanem@ensia.edu.dz>
2026-02-11 08:20:59 +0100

    added a health-check endpoint

--- findar_backend/api/views.py
+++ findar_backend/api/views.py
@@ -30,6 +30,14 @@ from .services import *
     @permission_classes([IsAuthenticated])
 """
 
+######### Health-check ##########
+@api_view(['GET'])
+def health_check():
+    #a small endpoint used for keep-alive
+    dummy_post = Post.objects.all()[:3] # get 3 posts
+    return Response({"message": "Successully fetched the health-check endpoint "}, status = status.HTTP_200_OK)
+
+
 #########  LISTINGS VIEW  #########
 
 
